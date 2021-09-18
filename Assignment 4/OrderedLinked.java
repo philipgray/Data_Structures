3cb146a7d718c111fa32ed1e45f1c594c2ed01f3
Philip Gray <philiprgray@msn.com>
2021-09-18 08:50:10 -0400

    fixed add thanks isaac!

--- Assignment 4/OrderedLinked.java
+++ Assignment 4/OrderedLinked.java
@@ -1,21 +1,91 @@
 public class OrderedLinked <T extends Comparable> extends DoubleLinked {
     public void insert(T newData) {
         if (this.start == null) {
-            start = new DNode(newData);
-            start.setPrevious(null);
-            start.setNext(null);
+            start = new DNode(newData, null, null);
+            end = start;
         } else {
             DNode current = start;
-            while (newData < current.getData() && current.getNext() != null) {
-                current = current.getNext();
-            }
-            if (current.getNext() == null) {
-                current.setNext(new DNode(newData, current, null));
+            System.out.println("New Data:" + newData+"\nCurrent: "+current.getData());
+            System.out.println(newData.compareTo(current.getData()));
+
+            if (newData.compareTo(current.getData()) <= 0) {
+                // if our newData is less than current or equal to
+                if (current.getPrevious() == null) {
+                    // if newData is less or equal to than the first item in our list
+                    current.setPrevious(new DNode(newData, null, current));
+                    start = current.getPrevious();
+                    len++;
+                } else {
+                    // if newData is less than or equal to an item in our list
+                    DNode previous = current.getPrevious();
+                    previous.setNext(new DNode(newData, previous, current));
+                    current.setPrevious(previous.getNext());
+                    len++;
+                }
             } else {
-                DNode next = current.getNext();
-                current.setNext(new DNode(newData, current, next));
-                next.setPrevious(current.getNext());
+                while (newData.compareTo(current.getData()) > 0) {
+                    // looping until our item is no longer greater than
+                    if (current.getNext() != null) {
+                        current = current.getNext();
+                    } else {
+                        // we've reached the end of our list with nothing greater than
+                        break; // maybe bad practice?
+                    }
+                }
+                if (current.getNext() == null) {
+                    current.setNext(new DNode(newData, current, null));
+                    end = current.getNext();
+                    len++;
+                } else {
+                    DNode next = current.getNext();
+                    current.setNext(new DNode(newData, current, next));
+                    next.setPrevious(current.getNext());
+                    len++;
+                }
+            }
+
+        }
+    }
+
+    public boolean remove(T newData) {
+        DNode current = start;
+        boolean didWeRemove = false;
+        if (current != null) {
+            while (newData.compareTo(current.getData()) != 0) {
+                if (current.getNext() != null) {
+                    current = current.getNext();
+                } else {
+                    // if we run out of items to compare to
+                    break;
+                }
+            }
+            if (newData.compareTo(current.getData()) == 0) {
+                if (current.getPrevious() == null) {
+                    // we're removing the first item in our list
+                    if (current.getNext() != null) {
+                        start = current.getNext();
+                        start.setPrevious(null);
+                    } else {
+                        // There was only one item in our list
+                        start = null;
+                        end = null;
+                    }
+                } else {
+                    if (current.getNext() != null) {
+                        current.getPrevious().setNext(current.getNext());
+                        current.getNext().setPrevious(current.getPrevious());
+                    } else {
+                        current.getPrevious().setNext(null);
+                        end = null;
+                    }
+                }
+                didWeRemove = true;
+
             }
         }
+        if (didWeRemove) {
+            len--;
+        }
+        return didWeRemove;
     }
 }
--- Assignment 4/testingClass.java
+++ Assignment 4/testingClass.java
@@ -1,16 +1,31 @@
 public class testingClass {
-    public static void testInsert() {
-        OrderedLinked<Integer> testInserting = new OrderedLinked<Integer>();
+    public static void testInsert(OrderedLinked<Integer> insert_list_here, Integer number) {
+        System.out.println("Adding "+number+" to OrderedLinked");
+        insert_list_here.insert(number);
+        System.out.println("Current size of our OrderedLinked: "+insert_list_here.getSize());
+        insert_list_here.print();
+    }
 
-        testInserting.insert(5);
-        testInserting.insert(11);
-        testInserting.insert(1);
-        testInserting.insert(2);
-
-        testInserting.print();
+    public static void testRemove(OrderedLinked<Integer> insert_list_here, Integer number) {
+        System.out.println("Removing "+number+" from OrderedLinked");
+        insert_list_here.remove(number);
+        System.out.println("Current size of our OrderedLinked: "+insert_list_here.getSize());
+        insert_list_here.print();
     }
 
     public static void main(String[] args) {
-        testInsert();
+        OrderedLinked<Integer> newIntegerList = new OrderedLinked<>();
+        testInsert(newIntegerList, 5);
+        testInsert(newIntegerList, 3);
+        testInsert(newIntegerList, 2);
+        testInsert(newIntegerList, 6);
+        testInsert(newIntegerList, 1);
+        testInsert(newIntegerList, 1);
+        testInsert(newIntegerList, 6);
+        testInsert(newIntegerList, 0);
+
+        testRemove(newIntegerList, 0);
+        testRemove(newIntegerList, 3);
+        testRemove(newIntegerList, 3);
     }
 }
