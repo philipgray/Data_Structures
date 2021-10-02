a6f27e192fa44493c8dbe6d16ea1728081aea034
Philip <philiprgray@msn.com>
2021-10-02 19:11:54 -0400

    small updates

--- Assignment 5/SkipList.java
+++ Assignment 5/SkipList.java
@@ -1,6 +1,7 @@
 public class SkipList <T extends Comparable> implements SearchList {
 
     SkipTower skipListTower;
+    int len = 0;
 
     public SkipList() {
         this.skipListTower = new SkipTower();
@@ -12,6 +13,7 @@ public class SkipList <T extends Comparable> implements SearchList {
 
             skipListTower.setTop(new SkipNode(newData));
             skipListTower.setBottom(skipListTower.getTop());
+            len++;
 
         } else {
 
@@ -21,13 +23,29 @@ public class SkipList <T extends Comparable> implements SearchList {
             while (operationIncomplete) {
 
                 if (newData.compareTo(current.getData()) <= 0) {
-                    // if our new data
+                    // if our new data is less than current data
 
                     if (current.getWest() == null) {
+                        // if oue newData is less than current data
                         current.setWest(new SkipNode(newData, null, current));
-                    }
+                        skipListTower.setTop(current.getWest());
+                    } else {
+                        // if newData is less than or equal to an item in our list
+                        SkipNode west = current.getWest();
+                        west.setEast(new SkipNode(newData, west, current));
+                        current.setWest(west.getEast());
 
-                } // if (newData compared to current)
+                    }
+                    len++;
+                    operationIncomplete = false;
+
+                } else if (current.getWest() == null) {
+                    current.setWest(new SkipNode(newData, current));
+                    len++;
+                } else {
+                    current = current.getWest();
+                }
+                // if
 
             } // while (operationIncomplete)
         } // else
